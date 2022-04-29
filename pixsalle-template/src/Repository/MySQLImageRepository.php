ce281b88ec662a712c73952d6c2e2cf9a374718e
jllob <j.llobetruiz@gmail.com>
2022-04-29 18:41:02 +0200

    getImages function on ImageRepository

--- pixsalle-template/src/Repository/MySQLImageRepository.php
+++ pixsalle-template/src/Repository/MySQLImageRepository.php
@@ -7,6 +7,20 @@ class MySQLImageRepository implements ImageRepository
 
     public function getImages()
     {
+        $query = <<<'QUERY'
+        
+        SELECT * FROM images;   
+        QUERY;
 
+        $statement = $this->databaseConnection->prepare($query);
+
+        $statement->execute();
+
+        $count = $statement->rowCount();
+
+        if ($count > 0) {
+            return $statement->fetch(PDO::FETCH_OBJ);
+        }
+        return null;
     }
 }
