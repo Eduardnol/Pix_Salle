8280f233f7b480bf7a9553468a5dc24b123d0312
Eduardo <eduardnol@gmail.com>
2022-04-29 19:54:38 +0200

    Membership controller finished

--- pixsalle-template/src/Controller/MembershipController.php
+++ pixsalle-template/src/Controller/MembershipController.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace Salle\PixSalle\Controller;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Salle\PixSalle\Repository\MembershipRepository;
 use Salle\PixSalle\Service\ValidatorService;
+use Slim\Routing\RouteContext;
 use Slim\Views\Twig;
 
 class MembershipController
@@ -23,15 +26,45 @@ class MembershipController
 		$this->validator = new ValidatorService();
 	}
 
-	public function showActualMembership(int $id)
+	public function showMembership(Request $request, Response $response): Response
 	{
+		$routeParser = RouteContext::fromRequest($request)->getRouteParser();
+
+		return $this->twig->render(
+			$response,
+			'membership.twig',
+			[
+				'formAction' => $routeParser->urlFor('membership')
+			]
+		);
+	}
+
+	public function showActualMembership()
+
+	{
+		$id = $_SESSION['user_id'];
 		return $this->membershipRepository->showCurrentMembership($id);
 
 	}
 
-	public function changeCurrentMembership(int $userId, bool $isActive)
+	public function changeCurrentMembership(Request $request, Response $response): Response
 	{
+		$data = $request->getParsedBody();
+		$routeParser = RouteContext::fromRequest($request)->getRouteParser();
+		$isActive = $data['isActive'];
+		$userId = $_SESSION['user_id'];
 		$this->membershipRepository->changeCurrentMembership($userId, $isActive);
+
+		//TODO: change the twig page
+		return $this->twig->render(
+			$response,
+			'membership.twig',
+			[
+				'formErrors' => $errors,
+				'formData' => $data,
+				'formAction' => $routeParser->urlFor('signUp')
+			]
+		);
 	}
 
 
