c096547645e31a07e9d32a1ac247d9261593412b
Eduardo <eduardnol@gmail.com>
2022-04-29 19:35:55 +0200

    Added constructor arguments

--- pixsalle-template/src/Controller/MembershipController.php
+++ pixsalle-template/src/Controller/MembershipController.php
@@ -3,8 +3,33 @@ declare(strict_types=1);
 
 namespace Salle\PixSalle\Controller;
 
-class MembershipController
-{
-	private bool $isActiveMemebership;
+use Salle\PixSalle\Repository\MembershipRepository;
+use Salle\PixSalle\Service\ValidatorService;
+use Slim\Views\Twig;
 
+class MembershipController implements MembershipRepository
+{
+	private Twig $twig;
+	private ValidatorService $validator;
+	private MembershipRepository $membershipRepository;
+
+	public function __construct(
+		Twig                 $twig,
+		MembershipRepository $membershipRepository
+	)
+	{
+		$this->twig = $twig;
+		$this->membershipRepository = $membershipRepository;
+		$this->validator = new ValidatorService();
+	}
+
+	public function showCurrentMembership(int $userId)
+	{
+		// TODO: Implement showCurrentMembership() method.
+	}
+
+	public function changeCurrentMembership(int $userId)
+	{
+		// TODO: Implement changeCurrentMembership() method.
+	}
 }
