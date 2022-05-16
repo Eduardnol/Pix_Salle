23cc393f5ec01b6f83a4303c45527a38c042fb7b
Eduardo <eduardnol@gmail.com>
2022-05-16 22:35:48 +0200

    Updated dependencies.php

--- pixsalle-template/config/dependencies.php
+++ pixsalle-template/config/dependencies.php
@@ -3,15 +3,17 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Salle\PixSalle\Controller\ChangePasswordController;
 use Salle\PixSalle\Controller\ExploreController;
 use Salle\PixSalle\Controller\MembershipController;
-use Salle\PixSalle\Controller\ChangePasswordController;
+use Salle\PixSalle\Controller\PortfolioController;
 use Salle\PixSalle\Controller\ProfileController;
 use Salle\PixSalle\Controller\SignUpController;
 use Salle\PixSalle\Controller\UserSessionController;
-use Salle\PixSalle\Repository\MySQLImageRepository;
 use Salle\PixSalle\Controller\WalletController;
+use Salle\PixSalle\Repository\MySQLImageRepository;
 use Salle\PixSalle\Repository\MySQLMembership;
+use Salle\PixSalle\Repository\MySQLPortfolioRepository;
 use Salle\PixSalle\Repository\MySQLUserRepository;
 use Salle\PixSalle\Repository\MySQLWalletRepository;
 use Salle\PixSalle\Repository\PDOConnectionBuilder;
@@ -41,16 +43,19 @@ function addDependencies(ContainerInterface $container): void
 		return new MySQLUserRepository($container->get('db'));
 	});
 
-    $container->set('image_repository', function (ContainerInterface $container) {
-        return new MySQLImageRepository($container->get('db'));
-    });
+	$container->set('image_repository', function (ContainerInterface $container) {
+		return new MySQLImageRepository($container->get('db'));
+	});
+	$container->set('portfolio_repository', function (ContainerInterface $container) {
+		return new MySQLPortfolioRepository($container->get('db'));
+	});
 
-    $container->set(
-        UserSessionController::class,
-        function (ContainerInterface $c) {
-            return new UserSessionController($c->get('view'), $c->get('user_repository'));
-        }
-    );
+	$container->set(
+		UserSessionController::class,
+		function (ContainerInterface $c) {
+			return new UserSessionController($c->get('view'), $c->get('user_repository'));
+		}
+	);
 	$container->set('wallet_repository', function (ContainerInterface $container) {
 		return new MySQLWalletRepository($container->get('db'));
 	});
@@ -58,18 +63,18 @@ function addDependencies(ContainerInterface $container): void
 		return new MySQLMembership($container->get('db'));
 	});
 
-    $container->set(
-        SignUpController::class,
-        function (ContainerInterface $c) {
-            return new SignUpController($c->get('view'), $c->get('user_repository'));
-        }
-    );
-    $container->set(
-        ExploreController::class,
-        function (ContainerInterface $c) {
-            return new ExploreController($c->get('view'), $c->get('image_repository'));
-        }
-    );
+	$container->set(
+		SignUpController::class,
+		function (ContainerInterface $c) {
+			return new SignUpController($c->get('view'), $c->get('user_repository'));
+		}
+	);
+	$container->set(
+		ExploreController::class,
+		function (ContainerInterface $c) {
+			return new ExploreController($c->get('view'), $c->get('image_repository'));
+		}
+	);
 	$container->set(
 		WalletController::class,
 		function (ContainerInterface $c) {
@@ -83,17 +88,23 @@ function addDependencies(ContainerInterface $container): void
 		}
 	);
 
-    $container->set(
-        ProfileController::class,
-        function (ContainerInterface $c) {
-            return new ProfileController($c->get('view'), $c->get('user_repository'));
-        }
-    );
+	$container->set(
+		ProfileController::class,
+		function (ContainerInterface $c) {
+			return new ProfileController($c->get('view'), $c->get('user_repository'));
+		}
+	);
 
-    $container->set(
-        ChangePasswordController::class,
-        function (ContainerInterface $c) {
-            return new ChangePasswordController($c->get('view'), $c->get('user_repository'));
-        }
-    );
+	$container->set(
+		ChangePasswordController::class,
+		function (ContainerInterface $c) {
+			return new ChangePasswordController($c->get('view'), $c->get('user_repository'));
+		}
+	);
+	$container->set(
+		PortfolioController::class,
+		function (ContainerInterface $c) {
+			return new PortfolioController($c->get('view'), $c->get('portfolio_repository'));
+		}
+	);
 }
