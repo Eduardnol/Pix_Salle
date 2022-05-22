28b5f18cf217bfa7a4695ba39554103be31e8457
Eduardo <eduardnol@gmail.com>
2022-05-22 12:07:03 +0200

    Grid in portfolio and default username in register

--- pixsalle-template/src/Repository/MySQLImageRepository.php
+++ pixsalle-template/src/Repository/MySQLImageRepository.php
@@ -39,27 +39,14 @@ final class MySQLImageRepository implements ImageRepository
 		$aux = 1;
 		$images = [];
 		$query = <<<'QUERY'
-        SELECT i.imagePath,u.userName FROM images as i, users as u where i.userId = u.id;   
+        SELECT i.imagePath,u.userName FROM images as i INNER JOIN users as u ON i.userId = u.id;   
         QUERY;
 
 		$statement = $this->databaseConnection->prepare($query);
 
 		$statement->execute();
-
-		while ($row = $statement->fetch()) {
-			$aux2 = 1;
-			$images[$aux][$aux2] = array($row['imagePath']);
-			$aux2 = 2;
-
-			if (array($row['userName']) == NULL) {
-				$images[$aux][$aux2] = 'user' . $_SESSION['user_id'];
-			} else {
-				$images[$aux][$aux2] = array($row['userName']);
-			}
-			$aux = $aux + 1;
-		}
-
-		return $images;
+		$final = $statement->fetchAll();
+		return $final;
 	}
 
 }
